9d61a843cf7a987ea85c8797c9956812d64a4b68
Luíza Rubio <39441794+luizarubio@users.noreply.github.com>
2023-06-11 19:39:26 +0200

    Update and rename Dashboard.py to app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+import dash
+from dash import html, dcc
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Tabs([
+        dcc.Tab(label='Distribuição Geográfica', children=[
+            html.Iframe(src='bars_mun_sc_2022.html', width='100%', height='500'),
+            html.Iframe(src='mapa_idade_media_2022.html', width='100%', height='500'),
+            html.Iframe(src='mapa_idade_media_2020.html', width='100%', height='500')
+        ]),
+        dcc.Tab(label='Grau de Escolaridade', children=[
+            html.Iframe(src='mapa_escolaridade_mun_2016.html', width='100%', height='500'),
+            html.Iframe(src='mapa_escolaridade_mun_2018.html', width='100%', height='500'),
+            html.Iframe(src='mapa_escolaridade_mun_2020.html', width='100%', height='500')
+        ]),
+        dcc.Tab(label='Idade Média', children=[
+            html.Iframe(src='mapa_idade_media_2014.html', width='100%', height='500'),
+            html.Iframe(src='mapa_idade_media_2016.html', width='100%', height='500'),
+            html.Iframe(src='mapa_idade_media_2018.html', width='100%', height='500')
+        ])
+    ])
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
